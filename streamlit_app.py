52f3d797e91e8495778dc891d5bc8cd58080021b
kakauz11 <kbeatriz856@icloud.com>
2025-12-05 18:01:29 +0000

    Primeiro commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Gabriela em uma aventura pelo centro de Assú")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
